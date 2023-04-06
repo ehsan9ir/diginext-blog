a0f9829b1d2017d126173d2b71de5b5d9c6cfe43
Ehsan9 <h.ehsan24@gmail.com>
2023-04-06 18:38:38 +0330

    feat: implement User factory and User seeder

---
+++ Modules/User/Database/factories/UserFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Modules\User\Database\factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class UserFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = \Modules\User\Entities\User::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'username' => $this->faker->unique()->userName(),
+            'email_verified_at' => now(),
+            'password' => bcrypt($this->faker->password), // password
+            'remember_token' => Str::random(10),
+        ];
+    }
+}
+
